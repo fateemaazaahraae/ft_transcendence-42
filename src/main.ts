be84eb4a8b5e43964eb18040750fae5da1a31593
fbazaz <fbazaz@vmc4.myguest.virtualbox.org>
2025-10-06 18:07:44 +0100

    fixing index.html with fixed components (background & cursor & logo)

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import "./../styles/index.css";
 import Landing from "./pages/landing";
 // import Login from "./pages/login";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = Landing();
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Landing();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Login();
