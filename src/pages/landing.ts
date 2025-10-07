eb86e17533af63bc31ad6ac4527214786d5c5089
Keltoum Nacer <knacer@c4r10p1.1337.ma>
2025-10-07 08:48:42 +0100

    working on landing page

--- src/pages/landing.ts
+++ src/pages/landing.ts
@@ -1,5 +1,14 @@
 export default function Landing() {
   return `
-    <h1 style="color: white">PING PONG LEGENDS</h1>
+    <div class="w-full flex flex-col items-center pt-20">
+      <h1 class="text-black text-8xl font-glitch  text-center drop-shadow-[0_0_20px_#35C6DD]" style="-webkit-text-stroke: 3px rgba(53, 198, 221, 0.6);">
+        PING PONG <br> LEGENDS
+      </h1>
+      <img src="/images/table1.png" class="w-auto max-w-[600px] z-10">
+      <button class="font-glitch mt-6 px-6 py-3 text-2xl bg-black text-white rounded-3xl drop-shadow-[0_0_10px_#35C6DD]">
+        Play now
+      </button>
+    </div>
   `;
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {
       fontFamily: {
-        glitch: ["Rubik Glitch", "cursive"],
+        glitch: ["'Rubik Glitch'", "cursive"],
+      },
+       dropShadow: {
+        cyan: "0 0 10px #35C6DD", // custom drop shadow
       },
     },
   },
