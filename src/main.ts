46e16aa0de25bfaa5fe1bdb71d003426b0cecc6e
bbenjrai <bbenjrai@e1r13p2.1337.ma>
2025-10-07 14:58:24 +0100

    creating chat branch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import "./../styles/index.css";
 import Landing from "./pages/landing";
+// import Chat from "./pages/Chat";
 // import Login from "./pages/login";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = Landing();
+
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Chat();
+
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Login();
