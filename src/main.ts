9639c3cc7aff3d91169677f9394a90ce724cc4f7
Keltoum Nacer <knacer@c4r8p5.1337.ma>
2025-10-14 15:21:48 +0100

    working on media query

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ import Settings from "./pages/settings";
 import Friends from "./pages/friends";
 import gameStyle from "./pages/gameStyle";
 
-// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Landing();
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = Landing();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Home();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = GameStyle();
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = Game();
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = Game();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Login();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Leaderboard();
 // document.querySelector<HTMLDivElement>("#app")!.innerHTML = Settings();
--- src/pages/landing.ts
+++ src/pages/landing.ts
@@ -1,22 +1,22 @@
 export default function Landing() {
   return `
-    <div class="w-full flex flex-col items-center pt-40">
+    <div class="w-full h-full flex flex-col items-center pt-40">
     <div class="relative text-center">
-      <h1 class="text-black  text-9xl font-glitch  text-center text-shadow-cyan leading-[1.1]"
+      <h1 class="text-black text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-glitch text-center text-shadow-cyan leading-[1.1]"
       style="-webkit-text-stroke: 5px rgba(53,198,221,0.6);">
         PING PONG <br>
         LEGENDS
         </h1>
-        <div class="absolute left-1/2 top-[52%] -translate-x-1/2 -translate-y-1/2 w-[900px] h-[22px] bg-[rgba(53,198,221)] blur-[12px] opacity-100 rounded-full"> </div>
+        <div class="absolute left-1/2 top-[52%] -translate-x-1/2 -translate-y-1/2 w-[400px] h-[9px] xl:w-[900px] xl:h-[22px] bg-[rgba(53,198,221)] blur-[12px] opacity-100 rounded-full"> </div>
       </div>
-      <div class="w-full flex gap-[10px] justify-center mt-15 items-center ">
-      <img src="/images/dark-girl.svg" class="drop-shadow-blue w-auto max-w-[400px] ">
-      <img src="/images/table1.png" class="mt-20 w-auto max-w-[600px] ">
-      <img src="/images/white-boy2.svg" class=" drop-shadow-blue w-auto max-w-[450px]">
+      <div class="w-full flex xl:gap-[10px] justify-center mt-15 items-center ">
+      <img src="/images/dark-girl.svg" class="drop-shadow-blue opacity-0 md:opacity-100 md:w-auto md:max-w-[170px] lg:max-w-[220px] xl:max-w-[400px] ">
+      <img src="/images/table1.png" class="mt-20 w-auto max-w-[320px] lg:max-w-[500px] xl:max-w-[600px] ">
+      <img src="/images/white-boy2.svg" class=" drop-shadow-blue opacity-0 md:opacity-100 md:w-auto md:max-w-[180px] lg:max-w-[220px] xl:max-w-[450px]">
       </div>
      <div class="relative inline-block group">
       <button 
-        class="mt-10 font-glitch px-12 py-3 text-2xl bg-black text-white rounded-[50px] 
+        class="mt-10 font-glitch px-8 lg:px-12 py-3 text-xl bg-black text-white rounded-[50px] 
               drop-shadow-cyan transition-all duration-300 
               group-hover:opacity-0 group-hover:scale-0">
         Play now
@@ -27,42 +27,43 @@ export default function Landing() {
               transition-all duration-300 group-hover:opacity-100 group-hover:scale-100" 
       />
       </div>
-      <h1 class="mt-[200px] text-white font-glitch text-4xl">Think you've got the reflexes to keep up? </h1>
-      <p class="text-white font-roboto text-xl text-center mt-10">
+      <h1 class=" mt-[25%] lg:mt-[180px] xl:mt-[200px] text-white font-glitch text-[18px] md:text-xl lg:text-3xl xl:text-4xl">Think you've got the reflexes to keep up? </h1>
+      <p class="text-white font-roboto text-[14px] md:text-[15px] lg:text-xl text-center pl-[5%] pr-[5%] mt-7 lg:mt-10">
       Step into the thrilling world of ping pong where speed meets strategy.<br/>
       From first-timers to seasoned pros, everyone can dive in and enjoy nonstop<br/>
       action. Challenge the AI in solo mode or face off against friends in intense<br/>
       multiplayer battles. With smooth controls, adjustable difficulty, and fast, dynamic<br/>
       gameplay, every rally will push you to the limit!
       </p>
-      <div class="mt-[200px] w-full gap-5 flex justify-center items-center">
-      <img src="/images/purple-girl.svg" class="drop-shadow-purple w-auto max-w-[400px] -translate-y-[150px]">
-      <img src="/images/red-boy.svg" class=" drop-shadow-red w-auto max-w-[450px]">
-      <img src="/images/green-girl.svg" class="drop-shadow-green w-auto max-w-[400px] -translate-y-[150px]">
+      <div class="mt-[30%] md:mt-[20%] xl:mt-[200px] w-full gap-2 xl:gap-5 flex justify-center items-center">
+      <img src="/images/purple-girl.svg" class="drop-shadow-purple w-auto max-w-[150px] md:max-w-[280px] lg:max-w-[350px] xl:max-w-[400px] -translate-y-[80px] lg:-translate-y-[120px] xl:-translate-y-[150px]">
+      <img src="/images/red-boy.svg" class=" drop-shadow-red w-auto max-w-[180px] md:max-w-[280px] lg:max-w-[350px] xl:max-w-[450px]">
+      <img src="/images/green-girl.svg" class="drop-shadow-green w-auto max-w-[160px] md:max-w-[280px] lg:max-w-[350px] xl:max-w-[400px] -translate-y-[80px] lg:-translate-y-[120px] xl:-translate-y-[150px]">
       </div>
       
-      <h1 class=" text-black font-glitch text-6xl mt-[200px] text-shadow-white"
+      <h1 class=" text-black font-glitch text-3xl md:text-4xl lg:text-6xl mt-[25%] md:mt-[20%] xl:mt-[200px] text-shadow-white"
           style="-webkit-text-stroke: 2.5px rgba(255,255,255, 0.9);">Owners</h1>
-      <div class="w-full gap-[100px] flex justify-center items-center  mt-20 pb-20">
+      <div class="w-full gap-[5%] xl:gap-[100px] flex justify-center items-center mt-[10%]  xl:mt-20 pb-10 lg:pb-20">
       <!-- SALMA -->
       <div class="relative flex flex-col group">
       <img src="/images/salma1.png" 
-            class="w-auto max-w-[500px] transition-all duration-300 group-hover:blur-[10px]" />
+            class="w-auto max-w-[80px] md:max-w-[150px] lg:max-w-[200px] xl:max-w-[500px] transition-all duration-300 group-hover:blur-[10px]" />
       <div class="absolute inset-0 flex flex-col items-center justify-center 
               opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-      <p class="text-white text-center text-xl font-glitch mb-4">Salma Alaoui</p>
-      <div class="flex text-white gap-3 text-2xl ">
-      <i class="fa-brands fa-github hover:text-[#FD8BD5] transition-colors"></i>
+      <p class="text-white text-center text-[8px] md:text-[14px] lg:text-xl font-glitch mb-1 xl:mb-4">Salma Alaoui</p>
+      <div class="flex text-white gap-1 xl:gap-3 text-[14px] md:text-[16px] lg:text-2xl ">
+      <i class="fa-brands fa-github  hover:text-[#FD8BD5] transition-colors"></i>
       <i class="fa-brands fa-linkedin hover:text-[#FD8BD5] transition-colors"></i>
       </div>
       </div>
       </div>
       <!-- FATI -->
       <div class="relative flex flex-col group">
-      <img src="/images/fati.png" class="w-auto max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
+      <img src="/images/fati.png" 
+            class="w-auto max-w-[80px] md:max-w-[150px] lg:max-w-[200px] xl:max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
       <div class="absolute inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 z-10">
-        <p class="text-white text-xl font-glitch mb-4">Fatima Zahrae Bazaz</p>
-        <div class="flex gap-3 text-white text-2xl">
+        <p class="text-white text-[8px] text-center md:text-[14px] lg:text-xl font-glitch mb-1 xl:mb-4">Fatima Zahrae Bazaz</p>
+        <div class="flex text-white gap-1 xl:gap-3 text-[14px] md:text-[16px] lg:text-2xl">
           <i class="fa-brands fa-github hover:text-[#00FFA8] transition-colors"></i>
           <i class="fa-brands fa-linkedin hover:text-[#00FFA8] transition-colors"></i>
         </div>
@@ -70,11 +71,12 @@ export default function Landing() {
       </div>
       <!-- KELTOUM -->
       <div class="relative flex flex-col group">
-      <img src="/images/keltoum1.png" class=" w-auto max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
+      <img src="/images/keltoum1.png" 
+            class=" w-auto lg:max-w-[200px] md:max-w-[150px] max-w-[80px] xl:max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
       <div class="absolute inset-0 flex flex-col items-center justify-center 
               opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-      <p class="text-white text-center text-xl font-glitch mb-4">Keltoum Nacer</p>
-      <div class="flex text-white gap-3 text-2xl ">
+      <p class="text-white text-center text-[8px] md:text-[14px] lg:text-xl font-glitch mb-1 xl:mb-4">Keltoum Nacer</p>
+      <div class="flex text-white gap-1 xl:gap-3 text-[14px] md:text-[16px] lg:text-2xl ">
       <i class="fa-brands fa-github hover:text-[#4D55AC] transition-colors"></i>
       <i class="fa-brands fa-linkedin hover:text-[#4D55AC] transition-colors"></i>
       </div>
@@ -82,11 +84,12 @@ export default function Landing() {
       </div>
       <!-- BOUCHRA -->
       <div class="relative flex flex-col group">
-      <img src="/images/boushra.png" class=" w-auto max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
+      <img src="/images/boushra.png" 
+            class=" w-auto lg:max-w-[200px] md:max-w-[150px] max-w-[80px] xl:max-w-[500px] transition-all duration-300 group-hover:blur-[10px]">
       <div class="absolute inset-0 flex flex-col items-center justify-center 
               opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-      <p class="text-white text-center text-xl font-glitch mb-4">Bouchra Benjraife</p>
-      <div class="flex text-white gap-3 text-2xl ">
+      <p class="text-white text-center text-[8px] md:text-[14px] lg:text-xl font-glitch mb-1 xl:mb-4">Bouchra Benjraife</p>
+      <div class="flex text-white gap-1 xl:gap-3 text-[14px] md:text-[16px] lg:text-2xl ">
       <i class="fa-brands fa-github hover:text-[#D82674] transition-colors"></i>
       <i class="fa-brands fa-linkedin hover:text-[#D82674] transition-colors"></i>
       </div>
