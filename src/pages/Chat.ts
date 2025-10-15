19627fc5cbf391ee7ff54745babeb59edd57854e
bbenjrai <bbenjrai@c4r5p1.1337.ma>
2025-10-15 12:46:33 +0100

    needs adding some icons &  ...

--- src/pages/Chat.ts
+++ src/pages/Chat.ts
@@ -36,21 +36,25 @@ export default function Chat() {
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-primary focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required />
+        <input type="search" id="default-search" class="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-primary focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required />
     </div>
     
       <div class="space-y-4">
           <div class="flex items-center gap-4 cursor-pointer hover:bg-blue-600 p-2 rounded">
-            <div class="w-10 h-10 bg-secondary rounded-full flex items-center justify-center text-black font-bold">A</div>
+            <div class="w-10 h-10 bg rounded-full flex items-center justify-center text-black font-bold">
+            <img src="../images/green-girl.svg" class="w-16 h-16 rounded-full right-">
+            </div>
             <div>
-              <p class="font-medium">Alice</p>
+              <p class="font-medium text-secondary ">Alice</p>
               <p class="text-sm text-gray-200">Hey, how are you?</p>
             </div>
           </div>
           <div class="flex items-center gap-4 cursor-pointer hover:bg-blue-600 p-2 rounded">
-            <div class="w-10 h-10 bg rounded-full flex items-center justify-center text-black font-bold">B</div>
+            <div class="w-10 h-10 bg rounded-full flex items-center justify-center text-black font-bold">
+            <img src="../images/white-boy.svg" class="w-16 h-16 rounded-full right-">
+            </div>
             <div>
-              <p class="font-medium">Bob</p>
+              <p class="font-medium text-secondary ">Bob</p>
               <p class="text-sm text-gray-200">Let's meet tomorrow</p>
             </div>
           </div>
