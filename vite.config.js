8aec35d767a818765905db9178ca0e90f8f4ab65
Keltoum Nacer <knacer@c4r10p1.1337.ma>
2025-10-17 16:25:29 +0100

    erge

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-  host: '0.0.0.0',
-  port: 5173,
-  allowedHosts: 'all',
-  fs: { strict: false },
-  strictPort: false,       // optional, allows auto port fallback
-  hmr: {
-    host: 'involuntarily-unsynthetic-erick.ngrok-free.dev', // ngrok URL
-    protocol: 'wss',        // use websocket secure for HMR
-  }
-},
-
+    allowedHosts: [
+      "involuntarily-unsynthetic-erick.ngrok-free.dev"
+    ],
+    host: true,
+    port: 5173,
+    fs: {
+      strict: false,
+    },
+  },
   build: {
     rollupOptions: {},
   },
