2ebaa9aa531d35c96ec1f1b35dd4ee471bcb47f2
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2025-10-19 09:06:40 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,82 @@
-// import "./../styles/index.css";
-// import Landing, { LandingEventListener } from "./pages/landing";
-// import Home, { HomeEventListener } from "./pages/home";
-// import GameStyle, { GameStyleEventListener } from "./pages/gameStyle.ts";
-// import Game from "./pages/game.ts";
-// import Login, { LoginEventListener } from "./pages/login";
-// import Register, { RegisterEventListener } from "./pages/register.ts";
-// import ResetPw, { ResetPwEventListener } from "./pages/resetpw.ts";
-// import TwoFactor, { FactorEventListener } from "./pages/TwoFactor.ts";
-// import ChangePw, { ChangePwEventListener } from "./pages/changepw.ts";
-// import ChoseAvatar, { ChoseAvatarEventListener } from "./pages/ChoseAvatar.ts";
-// import Leaderboard from "./pages/leaderboard";
-// import Settings from "./pages/settings.ts";
-// import Friends, {FriendsEventListener} from "./pages/friends.ts";
-// import Invitations, {InvitationsEventListener} from "./pages/invitaions.ts";
-// import Blocked, { BlockedEventListener } from "./pages/blocked.ts";
-// import PageNotFound from "./pages/pageNotFound.ts"
-// import { notifications, notificationBarListeners, renderNotifications } from "./pages/notifications.ts";
-// import { LanguagesMenuEventListener } from "./pages/languagesMenu.ts";
-// import { initLogout } from "./pages/logout.ts";
+import "./../styles/index.css";
+import Landing, { LandingEventListener } from "./pages/landing";
+import Home, { HomeEventListener } from "./pages/home";
+import GameStyle, { GameStyleEventListener } from "./pages/gameStyle.ts";
+import Game from "./pages/game.ts";
+import Login, { LoginEventListener } from "./pages/login";
+import Register, { RegisterEventListener } from "./pages/register.ts";
+import ResetPw, { ResetPwEventListener } from "./pages/resetpw.ts";
+import TwoFactor, { FactorEventListener } from "./pages/TwoFactor.ts";
+import ChangePw, { ChangePwEventListener } from "./pages/changepw.ts";
+import ChoseAvatar, { ChoseAvatarEventListener } from "./pages/ChoseAvatar.ts";
+import Leaderboard from "./pages/leaderboard";
+import Settings from "./pages/settings.ts";
+import Friends, {FriendsEventListener} from "./pages/friends.ts";
+import Invitations, {InvitationsEventListener} from "./pages/invitaions.ts";
+import Blocked, { BlockedEventListener } from "./pages/blocked.ts";
+import PageNotFound from "./pages/pageNotFound.ts"
+import { notifications, notificationBarListeners, renderNotifications } from "./pages/notifications.ts";
+import { LanguagesMenuEventListener } from "./pages/languagesMenu.ts";
+import { initLogout } from "./pages/logout.ts";
 // import { viewFriend } from "./pages/viewFriend.ts";
 
-// const routes: Record<string, {render: () => string; setUp?: () => void}> = {
-//     "/": {render: Landing, setUp: LandingEventListener},
-//     "/home": {render: Home, setUp: HomeEventListener},
-//     "/gameStyle": {render: GameStyle, setUp: GameStyleEventListener},
-//     "/game": {render: Game},
-//     "/login": {render: Login, setUp: LoginEventListener},
-//     "/register": {render: Register, setUp: RegisterEventListener},
-//     "/resetpw": {render: ResetPw, setUp: ResetPwEventListener},
-//     "/changepw": {render: ChangePw, setUp: ChangePwEventListener},
-//     "/TwoFactor": {render: TwoFactor, setUp:FactorEventListener},
-//     "/ChoseAvatar": {render: ChoseAvatar, setUp:ChoseAvatarEventListener},
-//     "/leaderboard": {render: Leaderboard},
-//     "/settings": {render: Settings},
-//     "/friends": {render: Friends, setUp: FriendsEventListener},
-//     "/invitations": {render: Invitations, setUp: InvitationsEventListener},
-//     "/blocked": {render: Blocked, setUp: BlockedEventListener},
-//     404: {render: PageNotFound},
-// };
+const routes: Record<string, {render: () => string; setUp?: () => void}> = {
+    "/": {render: Landing, setUp: LandingEventListener},
+    "/home": {render: Home, setUp: HomeEventListener},
+    "/gameStyle": {render: GameStyle, setUp: GameStyleEventListener},
+    "/game": {render: Game},
+    "/login": {render: Login, setUp: LoginEventListener},
+    "/register": {render: Register, setUp: RegisterEventListener},
+    "/resetpw": {render: ResetPw, setUp: ResetPwEventListener},
+    "/changepw": {render: ChangePw, setUp: ChangePwEventListener},
+    "/TwoFactor": {render: TwoFactor, setUp:FactorEventListener},
+    "/ChoseAvatar": {render: ChoseAvatar, setUp:ChoseAvatarEventListener},
+    "/leaderboard": {render: Leaderboard},
+    "/settings": {render: Settings},
+    "/friends": {render: Friends, setUp: FriendsEventListener},
+    "/invitations": {render: Invitations, setUp: InvitationsEventListener},
+    "/blocked": {render: Blocked, setUp: BlockedEventListener},
+    404: {render: PageNotFound},
+};
 
-// function render(path: string) {
-//     const app = document.querySelector<HTMLDivElement>("#app");
-//     const page = routes[path] || routes[404];
-//     app!.innerHTML = page.render();
+function render(path: string) {
+    const app = document.querySelector<HTMLDivElement>("#app");
+    const page = routes[path] || routes[404];
+    app!.innerHTML = page.render();
 
-//     requestAnimationFrame(() => {
-//         window.scrollTo(0, 0);
-//     });
-//     sideBarListeners();
-//     if (page.setUp)
-//         page.setUp();
-//     renderNotifications(notifications);
-//     initLogout();
-// }
+    requestAnimationFrame(() => {
+        window.scrollTo(0, 0);
+    });
+    sideBarListeners();
+    if (page.setUp)
+        page.setUp();
+    renderNotifications(notifications);
+    initLogout();
+}
 
-// function sideBarListeners() {
-//     const barIcons = document.querySelectorAll<HTMLElement>("aside i[data-path]");
-//     barIcons.forEach(icon => {
-//         icon.addEventListener("click", () => {
-//             const path = icon.dataset.path!;
-//             navigate(path);
-//         });
-//     });
-// }
+function sideBarListeners() {
+    const barIcons = document.querySelectorAll<HTMLElement>("aside i[data-path]");
+    barIcons.forEach(icon => {
+        icon.addEventListener("click", () => {
+            const path = icon.dataset.path!;
+            navigate(path);
+        });
+    });
+}
 
-// export function navigate(path: string) {
-//     console.log("rah 3eyto liya");
-//     window.history.pushState({}, "", path);
-//     render(path);
-// }
+export function navigate(path: string) {
+    console.log("rah 3eyto liya");
+    window.history.pushState({}, "", path);
+    render(path);
+}
 
-// window.addEventListener("popstate", () => {
-//     render(window.location.pathname);
-// })
+window.addEventListener("popstate", () => {
+    render(window.location.pathname);
+})
 
-// window.addEventListener("DOMContentLoaded", () => {
-//     render(window.location.pathname);
-//     notificationBarListeners();
-//     LanguagesMenuEventListener();
-//     viewFriend();
-// });
-
-import "./../styles/index.css";
-import cardFriend from "./pages/viewFriend";
-
-const app = document.querySelector<HTMLDivElement>("#app");
-app!.innerHTML = cardFriend();
+window.addEventListener("DOMContentLoaded", () => {
+    render(window.location.pathname);
+    notificationBarListeners();
+    LanguagesMenuEventListener();
+    // viewFriend();
+});
