2f15aae1e8d4c6d2e08f05b2a61f692a0329a8ca
Keltoum Nacer <knacer@c4r10p1.1337.ma>
2025-10-19 09:37:16 +0100

    logout is done

--- src/pages/pageNotFound.ts
+++ src/pages/pageNotFound.ts
@@ -1,9 +1,9 @@
 export default function PageNotFound() {
  return `
-  <div class="flex flex-col items-center  min-h-screen top-1/3 lg:top-1/4  text-white text-center relative overflow-hidden">
+  <div class="flex flex-col fixed items-center  min-h-screen top-1/3 left-[20%] xl:left-[30%] lg:top-1/4 text-white text-center overflow-x-hidden overflow-y-auto">
 
     <!-- Glitch 404 text -->
-    <h1 class="text-[80px] md:text-[120px] lg:text-[150px] font-glitch relative z-10  tracking-[8px] 
+    <h1 class="text-[80px] md:text-[120px] lg:text-[150px] font-glitch tracking-[8px] 
                drop-shadow-cyan
                before:content-['404'] before:absolute before:left-0 before:text-secondary 
                before:translate-x-[3px] before:top-0 before:animate-glitch 
@@ -14,7 +14,7 @@ export default function PageNotFound() {
 
     <!-- Subtitle -->
     <p class="text-3xl md:text-6xl lg:text-8xl mt-4 text-black drop-shadow-pink font-glitch z-10">Page Not Found</p>
-
+</div>
    
 `;
 
