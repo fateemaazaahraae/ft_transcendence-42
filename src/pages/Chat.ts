70ef260bb2062742fbe6cd296043bcbc9680208e
bbenjrai <bbenjrai@c4r3p3.1337.ma>
2025-10-30 22:16:27 +0100

    needs make it responsive & and check the scrollbar

--- src/pages/Chat.ts
+++ src/pages/Chat.ts
@@ -3,7 +3,7 @@
 export default function Chat() {
  
   return `
-  <div class="flex items-center justify-center text-white font-roboto px-6 md:px-20 py-6 relative flex flex-col">
+  <div class="class="h-screen overflow-hidden flex items-center justify-center text-white font-roboto px-6 md:px-20 py-6 relative flex flex-col">
 
     <!-- Sidebar -->
     <aside
@@ -24,6 +24,23 @@ export default function Chat() {
       <i class="fa-solid fa-gear text-[18px] text-primary hover:text-secondary transition-all duration-400 ease-in-out"></i>
     </aside>
   
+      <!-- Controls Icons -->
+    <div class="absolute top-10 right-[5%] flex items-center gap-5">
+      <div class="relative group">
+        <button class="flex items-center gap-2 text-primary font-roboto hover:text-secondary transition-all duration-400 ease-in-out">
+          <i class="fa-solid fa-chevron-down text-xs"></i>
+          En
+        </button>
+        <ul class="absolute mt-1 rounded-md hidden group-hover:block bg-black/80 p-2">
+          <li class="px-4 py-2 hover:text-secondary cursor-pointer transition-all duration-400 ease-in-out">En</li>
+          <li class=q"px-4 py-2 hover:text-secondary cursor-pointer transition-all duration-400 ease-in-out">Fr</li>
+          <li class="px-4 py-2 hover:text-secondary cursor-pointer transition-all duration-400 ease-in-out">Ar</li>
+        </ul>
+      </div>
+      <i class="fa-regular fa-bell text-primary hover:text-secondary cursor-pointer transition-all duration-400 ease-in-out"></i>
+      <i class="fa-solid fa-arrow-right-from-bracket text-primary hover:text-secondary cursor-pointer transition-all duration-400 ease-in-out"></i>
+    </div>
+
   <!-- chat side -->
 
  <div class="ml-[60px] h-screen w-screen flex items-center justify-center">
@@ -42,8 +59,8 @@ export default function Chat() {
           <input
             type="search"
             id="default-search"
-            class="block bg-primary/80 w-full h-[35px] px-4 ps-9 text-sm text-white rounded-2xl border-none focus:outline-none"
-            placeholder="Search ..."
+            class="block bg-primary/80 w-full h-[35px] px-4 ps-9 text-sm text-white rounded-2xl border-none focus:outline-none placeholder-white"
+            placeholder="Search ..." 
             required
           />
 
@@ -53,7 +70,10 @@ export default function Chat() {
       <div class="space-y-4 mt-3 mb-3 overflow-y-auto h-full scrollbar-thin scrollbar-thumb-secondary scrollbar-track-transparent">
           <div class="   scroll flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
             <div class="w-16 h-8 bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
             <img src="../images/green-girl.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+            <div class="absolute bottom-0 right-0 w-3 h-3 bg-greenAdd rounded-full"></div>
+            </div>
             </div>
             <div>
               <p class="font-medium text-secondary ">Bouchra</p>
@@ -61,8 +81,21 @@ export default function Chat() {
             </div>
           </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
+                <div class="relative w-12 h-12 flex-shrink-0">
+              <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full">
+              <div class="absolute bottom-0 right-0 w-3 h-3 bg-redRemove rounded-full"></div>
+            </div>
+            <div>
+              <p class="font-medium text-secondary">Boy</p>
+              <p class="text-sm text-gray-200">Let's meet tomorrow</p>
+            </div>
+            </div>
+            <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+              <div class="absolute bottom-0 right-0 w-3 h-3 bg-greenAdd rounded-full"></div>
+                </div>                
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -71,16 +104,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
-                </div>
-              <div>
-                <p class="font-medium text-secondary ">Boy</p>
-                <p class="text-sm text-gray-200">Let's meet tomorrow</p>
-              </div>
-            </div>
-            <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
-                <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
-                <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-redRemove rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -89,7 +116,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8 bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-greenAdd rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -98,7 +128,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-redRemove rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -107,7 +140,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-greenAdd rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -116,7 +152,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-redRemove rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -125,7 +164,10 @@ export default function Chat() {
             </div>
             <div class="flex items-center gap-4 cursor-pointer hover:bg-primary/65 p-2 rounded">
                 <div class="w-16 h-8  bg rounded-full flex items-center justify-center text-black font-bold">
+                <div class="relative w-12 h-12 flex-shrink-0">
                 <img src="../images/white-boy.svg" class="w-12 h-12 object-cover border border-primary rounded-full right-">
+                <div class="absolute bottom-0 right-0 w-3 h-3 bg-greenAdd rounded-full"></div>
+                </div> 
                 </div>
               <div>
                 <p class="font-medium text-secondary ">Boy</p>
@@ -144,7 +186,7 @@ export default function Chat() {
         <div class="flex items-center gap-3">
           <img src="../images/green-girl.svg" class="w-12 h-12 object-cover border border-primary rounded-full">
           <div>
-            <p class="font-medium">Bouchra</p>
+            <p class=" text-secondary font-bold ">Bouchra</p>
             <p class="text-sm text-gray-200">Online</p>
           </div>
         </div>
@@ -177,44 +219,50 @@ export default function Chat() {
 
         <!-- Messages -->
         <div class="flex-1 p-4 space-y-4 overflow-y-auto bg-primary/60">
-          <div class="flex justify-items ">
-          <img src="../images/green-girl.svg" class="w-14 h-14 object-cover mr-3 border border-primary rounded-full">
-        <div class="bg-primary/65 text-white  p-3 rounded-3xl w-fit  max-w-[60%]">
-           Hi Salma, how are you?
-           </div>
-           </div>
-
-           <div class="flex items-end gap-3 ml-auto w-fit">
-           <div class="bg-primary/65 text-white p-3 rounded-3xl w-fit  max-w-[70%]">Hey Bouchra! I'm doing great, thanks!</div>
-           <img src="../images/green-girl.svg" class="w-14 h-14  object-cover border border-primary rounded-full ">
-           </div>
-           
-           <div class="flex justify-items ">
-          <img src="../images/green-girl.svg" class="w-14 h-14 object-cover mr-3 border border-primary rounded-full ">
-          <div class="bg-primary/65 text-white p-3 rounded-3xl w-fit max-w-[70%]">Want to catch up later?</div>
-        </div>
+          
+          <!-- Message from other user (left side - receiver) -->
+          <div class="flex items-start">
+            <img src="../images/green-girl.svg" class="w-14 h-14 object-cover mr-3 border border-primary rounded-full flex-shrink-0">
+            <div class="bg-primary/65 text-white p-3 rounded-3xl max-w-[60%]">
+              Hi Salma, how are you?
+            </div>
+          </div>
+          
+          <!-- Message from current user (right side - sender) -->
+          <div class="flex items-start justify-end">
+            <div class="bg-primary/65 text-white p-3 rounded-3xl max-w-[60%]">
+              Hey Bouchra! I'm doing great, thanks!
+            </div>
+            <img src="../images/green-girl.svg" class="w-14 h-14 object-cover ml-3 border border-primary rounded-full flex-shrink-0">
+          </div>
+          
+          <!-- Message from other user (left side - receiver) -->
+          <div class="flex items-start">
+            <img src="../images/green-girl.svg" class="w-14 h-14 object-cover mr-3 border border-primary rounded-full flex-shrink-0">
+            <div class="bg-primary/65 text-white p-3 rounded-3xl max-w-[60%]">
+              Want to catch up later?
+            </div>
+          </div>
+          
         </div>
 
         <!-- Message Input -->
-          <div class="relative "  >
+          <div class="relative">
             <div class="p-4 bg-primary/60 rounded-b-xl">
-              <div class="relative ">
+              <div class="relative">
                 
-                <i class="w-4 h-4 fa-regular fa-paper-plane text-secondary absolute right-3 top-1/2 -translate-y-1/2"></i>
-                <i class="fa-solid fa-plus text-secondary absolute left-3 top-1/2 -translate-y-1/2" ></i>
-                <i class="fa-solid fa-table-tennis-paddle-ball text-secondary absolute left-9 top-1/2 -translate-y-1/2""></i>
-              <!-- <svg class="w-4 h-4 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                </svg>-->
+                <i class="fa-regular fa-paper-plane text-secondary absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-secondary absolute left-3 top-1/2 -translate-y-1/2 cursor-pointer"></i>
+                <i class="fa-solid fa-table-tennis-paddle-ball text-secondary absolute left-9 top-1/2 -translate-y-1/2 cursor-pointer"></i>
                 
-                <!-- Input with padding for icon -->
+                <!-- Input with padding for icons -->
                 <input 
                   type="text" 
-                  placeholder="       Type a message..." 
-                  class="w-full pl-10 pr-4 py-2 pr-10 rounded-full bg-primary/65 text-white placeholder-white focus:outline-none">
+                  placeholder="Type a message..." 
+                  class="w-full pl-16 pr-10 py-2 rounded-full bg-primary/65 text-white placeholder-white focus:outline-none">
               </div>
             </div>
-        </div>
+          </div>
         
    </div>
     </div>
@@ -254,10 +302,10 @@ export function closeChat()
     {
       chatDiv.innerHTML=`
         
-        <div class="flex-1  p-4 justify-items-center space-y-4 rounded-2xl overflow-y-auto bg-primary/60">
+         <div class="flex-1 flex items-center justify-center p-4 rounded-2xl overflow-y-auto bg-primary/60">
 
-          <h1 class=" "> Ping Pong Chat </h1>
-          </div>
+          <h1 class="text-center text-primary/65  font-bold text-4xl ">Ping Pong<br>Chat</h1>
+        </div>
         
       `;
     } 
@@ -265,6 +313,5 @@ export function closeChat()
     dropdown?.classList.add("hidden");
   });
   
-
 }
 
