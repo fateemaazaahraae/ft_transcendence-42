7306242b072f7041592ac83acdb6cf7e5952195f
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2025-10-30 09:30:36 +0100

    commit

--- backend/srcs/routes/register.js
+++ backend/srcs/routes/register.js
@@ -4,9 +4,9 @@ export function registerRoutes(fastify) {
   fastify.post("/register", async (request, reply) => {
     try {
       const body = request.body;
-      const firstName =body.firstName || "";
-      const lastName =body.lastName || "";
-      const userName =body.userName || "";
+      const firstName = body.firstName || "";
+      const lastName = body.lastName || "";
+      const userName = body.userName || "";
       const email = (body.email || "").toLowerCase();
       const password = body.password || "";
       const confirmPassword = body.confirmPassword || "";
@@ -19,7 +19,7 @@ export function registerRoutes(fastify) {
       if (emailExists) {
         return reply.code(409).send({ error: "Email already registered" });
       }
-      if(password !== body.confirmPassword)
+      if(password !== confirmPassword)
         return reply.code(400).send({error: "Passwords do not match"});
 
       const newuser = await createUser(firstName, lastName, userName, email, password);
