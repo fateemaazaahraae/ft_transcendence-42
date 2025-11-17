290b30b6506613845be6393965858022ce053495
Fatima-Zahrae <fatima-zahrae@Fatima-Zahraes-MacBook-Air.local>
2025-11-17 16:27:46 +0100

    done with authentication service

--- backend/auth-service/srcs/config/env.js
+++ backend/auth-service/srcs/config/env.js
@@ -3,7 +3,4 @@ import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// console.log("meta ---> " + import.meta.url)
-// console.log("file ---> " + fileURLToPath(import.meta.url))
-// console.log("dir ---> " + __dirname)
 dotenv.config({ path: resolve(__dirname, "../../.env") });
