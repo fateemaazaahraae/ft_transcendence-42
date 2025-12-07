4399d6d310f12ec03327a718fb40f0bec61013e5
bbenjrai <bbenjrai@c3r7p3.1337.ma>
2025-12-07 15:52:49 +0100

    still needs some changes

--- frontend/src/chat/types.ts
+++
@@ -1,9 +0,0 @@
-export type Message = {
-  id: string;
-  conversation_id: string;
-  sender_id: string;
-  content: string;
-  created_at: number;
-};
-
-export type NewMessageEvent = { conversation: any; message: Message };
--- frontend/src/config.ts
+++
@@ -1,4 +0,0 @@
-import config from "./config.json";
-
-export const API_URL = config.API_URL;
-export const CHAT_URL = config.CHAT_URL;
